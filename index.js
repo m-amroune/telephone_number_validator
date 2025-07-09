a4fd4549cedce651bb4e101882ab48f1e7cd3997
Moustapha <amroune.moustapha@gmail.com>
2025-07-09 14:11:36 +0200

    ending stylization

--- index.js
+++ index.js
@@ -5,22 +5,44 @@ const resultsDiv = document.querySelector("#results-div");
 
 
 
-
- 
-
 const regex = /^1?\s*(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
 
 
 
-
+// CHECK PHONE NUMBER VALIDATION
 const validatePhoneNumber = () => {
-    const phoneNumber = userInput.value.trim();
-
-    if(regex.test(phoneNumber)){
-        resultsDiv.textContent = `Valid US number: ${phoneNumber}`
+     const phoneNumber = userInput.value.trim();
+    
+    if (!phoneNumber) { 
+        alertInput();
+       
     } else{
-        resultsDiv.textContent = `Invalid US number: ${phoneNumber}`
+         checkPhoneNumber(phoneNumber);
+
+    }
+    
+   
+   
+}
+
+// if user click without entering a value
+const alertInput = () => {
+    alert("Please provide a phone number");
+}
+
+const checkPhoneNumber = (phoneNumber) => {
+    if (regex.test(phoneNumber)) {
+        resultsDiv.textContent = `Valid US number: ${phoneNumber}`;
+    } else {
+        resultsDiv.textContent = `Invalid US number: ${phoneNumber}`;
     }
 }
 
+
 checkBtn.addEventListener("click", validatePhoneNumber);
+
+// CLEAR INPUT AND REMOVED CONTENT
+clearBtn.addEventListener("click",  ()=>{
+    resultsDiv.textContent = "";
+    userInput.value = "";
+} ) 
