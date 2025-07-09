ff41a046b0595c516be3b71b3336ae7f4b23fd77
Moustapha <amroune.moustapha@gmail.com>
2025-07-09 11:40:52 +0200

    adding regex and arrow function for validation number

---
+++ index.js
@@ -0,0 +1,26 @@
+const userInput = document.querySelector("#user-input"); 
+const checkBtn = document.querySelector("#check-btn"); 
+const clearBtn = document.querySelector("#clear-btn");
+const resultsDiv = document.querySelector("#results-div");
+
+
+
+
+ 
+
+const regex = /^(1\s?)?(\(?\d{3}\)?[\s\-]?)?\d{3}[\s\-]?\d{4}$/
+
+
+
+
+validatePhoneNumber = () => {
+    const phoneNumber = userInput.value.trim();
+
+    if(regex.test(phoneNumber)){
+        resultsDiv.textContent = ` Valid US number : ${phoneNumber}  `
+    } else{
+        resultsDiv.textContent = ` Invalid US number : ${phoneNumber}  `
+    }
+}
+
+checkBtn.addEventListener("click", validatePhoneNumber);
